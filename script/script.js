c09da0fe50f4e544fed5785dba5bb3733577ae42
Christian Scherlipp <c.scherlipp@web.de>
2026-05-15 11:01:11 +0200

    feat: index.html, script.js, style and dialogstyle

--- script/script.js
+++ script/script.js
@@ -1,42 +1,43 @@
-let dogPicture = document.getElementById('content-dogGallery');
-let dialogDogPicture = document.getElementById('dialogPicture');
-let dialogTitle = document.getElementById('pictureTitle');
+    let dogPicture = document.getElementById('content-dogGallery'); // Variable mit zugriff auf den Content der Hauptseite 
+    let dialogDogPicture = document.getElementById('dialogPicture'); // Variable mit zugriff auf den main content des dialog
+    let dialogTitle = document.getElementById('pictureTitle'); // Variable mit zugriff auf den Titel des Dialog
 
-const dogGallery = ["Abby_Baden_ist_das_größte.jpeg",
-                    "Abby_Das_riecht_aber_gut.jpeg",
-                    "Abby_Lange_Auto_fahrten_machen_müde.jpeg",
-                    "Abby_spielen_in_der_freien_Natur.jpeg",
-                    "Kira_erter_Tag_im_neuen_zu_Hause.jpeg",
-                    "Kira_Es_gibt_Nach_wuchs.jpeg",
-                    "Kira_Im_Arm_schläft_sich_am_besten_.jpeg",
-                    "Kira_My_best_buddy_Sunny.jpeg",
-                    "Sunny_ein_gechillter_Tag.jpeg",
-                    "Sunny_Eis_ist_das_beste.jpeg",
-                    "Sunny_My_best_buddy_und_Ich_in_Errinnerung.jpeg",
-                    "Sunny_OH_es_gibt_was_neues.jpeg"
-]
-const dialogRef = document.getElementById('dialogGallery');
-
-
-
-function renderDogGallery() {
-    for (let i = 0; i < dogGallery.length; i++) {
-    dogPicture.innerHTML  += `<img id="dogGallery-Picture${i}" class="pictureDog" onclick="openDialog(${i})" src="./assets/img/gallery/${dogGallery[i]}" alt="${dogGallery[i]}">`;
-
-    }    
-}
+    const dogGallery = ["Abby_Baden_ist_das_größte.jpeg",
+                        "Abby_Das_riecht_aber_gut.jpeg",
+                        "Abby_Lange_Auto_fahrten_machen_müde.jpeg",
+                        "Abby_spielen_in_der_freien_Natur.jpeg",
+                        "Kira_erter_Tag_im_neuen_zu_Hause.jpeg",
+                        "Kira_Es_gibt_Nach_wuchs.jpeg",
+                        "Kira_Im_Arm_schläft_sich_am_besten_.jpeg",
+                        "Kira_My_best_buddy_Sunny.jpeg",
+                        "Sunny_ein_gechillter_Tag.jpeg",
+                        "Sunny_Eis_ist_das_beste.jpeg",
+                        "Sunny_My_best_buddy_und_Ich_in_Errinnerung.jpeg",
+                        "Sunny_OH_es_gibt_was_neues.jpeg"
+    ] // Liste mit den Pfaden zu den Bildern
+    const dialogRef = document.getElementById('dialogGallery'); // Variable mit zugriff auf den Dialog
 
 
-function openDialog(i) {
-    dialogRef.showModal();
-    dialogDogPicture.innerHTML = `<img  src="./assets/img/gallery/${dogGallery[i]}" alt="${dogGallery[i]}">`;
-    dialogTitle.innerHTML = `${dogGallery[i]}`;
-}
+    // Funktion zumm generieren der bilder im main der Hauptseite mit id, class und onclick funktion im main bereich des dialog 
+    function renderDogGallery() {
+        for (let i = 0; i < dogGallery.length; i++) {
+        dogPicture.innerHTML  += `<img id="dogGallery-Picture${i}" class="pictureDog" onclick="openDialog(${i})" tabindex = "0" src="./assets/img/gallery/${dogGallery[i]}" alt="${dogGallery[i]}">`;
+        
+        }    
+    }
 
+    // Dialog öffnen durch anklicken der bilder, Funktion zumm generieren der bilder im main des dialog sowie des titels
+    function openDialog(i) {
+        dialogRef.showModal();
+        dialogDogPicture.innerHTML = `<img  src="./assets/img/gallery/${dogGallery[i]}" alt="${dogGallery[i]}">`;
+        dialogTitle.innerHTML = `${dogGallery[i]}`; // generiert den Titel aus der Liste
+        dialogTitle.innerText = dogGallery[i].replaceAll("_", " ") .replaceAll(".jpeg", ""); // Sorgt dafür das der unterstrich und das .jpeg ersetzt werde 
+    }
 
-function closeDialog() {
-    dialogRef.close();
-}
+    //  Schließt den dialog
+    function closeDialog() {
+        dialogRef.close(); 
+    }
 
 
 
